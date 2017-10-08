7bddb780a4cd133c33cebe94846ae56694cd29ed
Teemu Räsänen <no-reply@no-reply.com>
2017-10-08 17:13:53 +0300

    Add simple room api, validation and show empty rooms on dashboard page

--- app/room/room.js
+++ app/room/room.js
@@ -16,9 +16,9 @@ let Room = class Room {
         json_agg(json_build_object('name', sensors.name, 'value', sd.value, 'type', sensors.type, 'unit', sensors.unit)) sensors,
         row_number() over (order by rooms.id) number
       FROM rooms
-      JOIN sensors
+      LEFT JOIN sensors
         ON rooms.id = sensors.room
-      JOIN sensor_data sd
+      LEFT JOIN sensor_data sd
         ON sensors.id = sd.sensor_id
       JOIN (
         SELECT
@@ -26,11 +26,17 @@ let Room = class Room {
           MAX(timestamp) as timestamp
         FROM sensor_data
         GROUP BY sensor_id
-      ) sd2 ON sd.sensor_id = sd2.sensor_id AND sd.timestamp = sd2.timestamp
+      ) sd2 ON sd.sensor_id = sd2.sensor_id AND sd.timestamp = sd2.timestamp or sensors.id is null
       GROUP BY rooms.id
       ORDER BY rooms.id
       OFFSET ${offset}
       LIMIT  ${pageSize}`);
+  }
+
+  static add(room) {
+    return db.query('INSERT INTO rooms (name) VALUES (${name})', {
+      name: room.name
+    });
   }
 };
 
--- app/room/routes.js
+++ app/room/routes.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var RoomPage = require('./room-page');
+let Room = require('./room');
 
+let validate = req => Number.isInteger(parseInt(req.params.pageNumber, 10));
 
 /* Main dashboard page. */
 router.get('/', function(req, res, next) {
@@ -11,7 +13,12 @@ router.get('/', function(req, res, next) {
     .catch(error => next(error));
 });
 
+/* Main dashboard page pagination */
 router.get('/:pageNumber', function(req, res, next) {
+  if (!validate(req)) {
+    next('route');
+    return;
+  }
   let roomPage = new RoomPage({
     pageNumber: parseInt(req.params.pageNumber, 10)
   });
@@ -20,7 +27,12 @@ router.get('/:pageNumber', function(req, res, next) {
     .catch(error => next(error));
 });
 
+/* Room detail view */
 router.get('/room/:pageNumber', function(req, res, next) {
+  if (!validate(req)) {
+    next('route');
+    return;
+  }
   let roomPage = new RoomPage({
     pageNumber: parseInt(req.params.pageNumber, 10),
     pageSize: 1
@@ -28,6 +40,16 @@ router.get('/room/:pageNumber', function(req, res, next) {
   roomPage.create()
     .then(() => roomPage.render(res))
     .catch(error => next(error));
+});
+
+/* Room API */
+router.post('/room/add', async function(req, res) {
+  await Room.add({
+    name: req.body.name
+  }).then(() => res.sendStatus(200)).catch(e => {
+    console.error(e);
+    res.sendStatus(500);
+  });
 });
 
 module.exports = router;
